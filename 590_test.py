9cdc8f270be52658e050064c5d39fac62f93a2f1
Melissa Horowitz <mrh56@duke.edu>
2018-09-11 16:00:04 -0400

    added program from class

---
+++ 590_test.py
@@ -0,0 +1,14 @@
+def main():
+	add_three(1,2,3)
+	divide_by_three(9)
+
+def add_three(v1, v2, v3):
+	p = v1+v2+v3
+	print(p)
+
+def divide_by_three(v1):
+	p = v1/3
+	print(p)
+
+if __name__ == "__main__":
+	main()
